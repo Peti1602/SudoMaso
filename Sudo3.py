4d7ca3adebfbc924d07bdb424f64c5f61926ba19
Noemi Szoke <szokenoemijob@gmail.com>
2018-05-03 13:46:14 +0200

    with colours

--- Sudo3.py
+++ Sudo3.py
@@ -66,11 +66,12 @@ joValasz = 0
 while a <= 8:
     x = int(error_handing("Válassz sort!: "))
     y = int(error_handing("Válassz oszlopot!: "))
-    z = str(error_handing("Mondj egy számot(1-9-ig): "))
+    z = str("\x1b[1;31m{}\x1b[0;37m" . format(input("Mondj egy számot(1-9-ig):")))
+
 
     if jateksor[x-1][y-1] == " ":
         a += 1
-        jateksor[x-1][y-1] = z
+        jateksor[x-1][y-1]=z
         screen()
         if z == sor[x-1][y-1]:
             joValasz += 1
